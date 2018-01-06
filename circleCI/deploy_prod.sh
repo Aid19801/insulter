a10522957b7b97f997bd486d77ff91858acda138
Adrian Thompson <adrian.thompson@sky.uk>
2018-01-06 09:58:23 +0000

    testing deploy script to hockeyapp

--- circleCI/deploy_prod.sh
+++ circleCI/deploy_prod.sh
@@ -1,3 +1,19 @@
 echo ====  deploy prod shell script ======
-ls
+cd android
+./gradlew assembleRelease
+cd app/build/outputs/apk/release
+pbcopy < app-release.apk
+
+echo we are currently at
+pwd
+echo =====
+
+curl
+-F “status=2”
+-F “notify=1”
+-F “ipa=@APK FULL PATH(android/build/outputs/apk/app-release.apk”
+-H “X-HockeyAppToken: $HOCKEYAPP_TOKEN”
+https://rink.hockeyapp.net/api/2/apps/$firstAppId/app_versions/upload7
+
+
 echo ==== deploy prod shell script ======
