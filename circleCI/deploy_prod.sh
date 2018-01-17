84079ef7ee82c505781670277c950f3b4c47d998
Adrian Thompson <adrian.thompson@sky.uk>
2018-01-17 17:11:02 +0000

    testing...

--- circleCI/deploy_prod.sh
+++ circleCI/deploy_prod.sh
@@ -1,6 +1,8 @@
 echo ====  deploy prod shell script ======
 echo === Android Home Variable is: ====
 echo $ANDROID_HOME
+echo FUBAR ===== blah blah blah 22222 =====
+echo $keyPassword
 pwd
 
 echo ==== deploy prod shell script ======
